b1c260ba262a7f3404f6308b923cb8b6b1a48deb
santan47 <santankumarsharma47@gmail.com>
2017-01-27 16:24:03 +0530

    c++ program.

---
+++ classfactorial.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class factorial
+{
+public:
+ int n,i,f;
+	void output();
+};
+void factorial::output()
+{
+int f=1,i,n;
+ cout<<"enter the no"<<endl;
+ cin>>n;
+
+for(i=1;i<=n;i++)
+f=f*i;
+cout<<"foctorial is:"<<f<<endl;
+}
+int main()
+{
+factorial fact;
+fact.output();
+return 0;
+}
